1772ba5883e41d827f337bd35cf212eaddeb1b42
jmberman12@gmail.com <jmberman12@gmail.com>
2024-11-02 08:27:01 +0000

    add payments and orders

---
+++ models/marts/finance/fct_orders.sql
@@ -0,0 +1,12 @@
+with orders as (
+    select *
+    from {{ ref('stg_jaffle_shop__orders') }}
+),
+payment as (
+    select *
+    from {{ ref('stg_stripe__payments') }}
+)
+
+select order_id, customer_id, order_date, amount
+from orders left join payment using (order_id)
+where orders.status = 'completed'
